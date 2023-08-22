90f38f83b7e0a7fb6160f95ff604d3a0204d07db
Claudia Zhao <claudiaxzhao@gmail.com>
2023-08-21 19:49:12 -0700

    new exercise

--- js/main.js
+++ js/main.js
@@ -1,41 +1,94 @@
-const team = {
-  _players: [
-    { firstName: "Pete", lastName: "Wheeler", age: 54 },
-    { firstName: "Sam", lastName: "Hilo", age: 50 },
-    { firstName: "Kona", lastName: "Wolff", age: 24 },
-  ],
-  _games: [
-    { opponent: "MU", teamPoints: 40, opponentPoints: 3 },
-    { opponent: "Chelsea", teamPoints: 0, opponentPoints: 20 },
-    { opponnt: "Greyhound", teamPoints: 10, opponentPoints: 10 },
-  ],
-  get players() {
-    return this._players;
-  },
-  get games() {
-    return this._games;
-  },
-  addPlayer(newFirstName, newLastName, newAge) {
-    let player = {
-      firstName: newFirstName,
-      lastName: newLastName,
-      age: newAge,
-    };
-    this.players.push(player);
-  },
-  addGame(opponent, teamPoints, opponentPoints) {
-    let game = {
-      opponent: opponent,
-      teamPoints: teamPoints,
-      opponentPoints: opponentPoints,
-    };
-    this.games.push(game);
-  },
-};
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+  get title() {
+    return this._title;
+  }
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+  get ratings() {
+    return this._ratings;
+  }
 
-team.addPlayer("Bad", "Bunny", 29);
+  set isCheckedOut(newIsCheckedOut) {
+    this._isCheckedOut = newIsCheckedOut;
+  }
 
-console.log(team._players);
+  getAverageRating() {
+    let ratingsSum = this._ratings.reduce(
+      (currentSum, rating) => currentSum + rating,
+      0
+    );
 
-team.addGame("Titans", 100, 98);
-console.log(team._games);
+    return Math.floor(ratingsSum / this._ratings.length);
+  }
+  addRatings(rating) {
+    if (rating <= 5) {
+      this._ratings.push(rating);
+    } else {
+      console.log("Rating have to be under 5");
+    }
+  }
+
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this._isCheckedOut;
+  }
+  addRating() {}
+}
+
+class Book extends Media {
+  constructor(author, title, pages) {
+    super(title);
+    this._author = author;
+    this._pages = pages;
+  }
+  get author() {
+    return this._author;
+  }
+  get pages() {
+    return this._pages;
+  }
+}
+
+class Movie extends Media {
+  constructor(director, title, runTime) {
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+
+  get director() {
+    return this._director;
+  }
+  get runTime() {
+    return this._runTime;
+  }
+}
+
+const historyOfEverything = new Book(
+  "Bill Bryson",
+  "A Short History of Nearly Everything",
+  544
+);
+
+historyOfEverything.toggleCheckOutStatus();
+
+console.log(historyOfEverything.isCheckedOut);
+
+historyOfEverything.addRatings(4);
+historyOfEverything.addRatings(5);
+historyOfEverything.addRatings(5);
+
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie("Jan de Bont", "Speed", 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRatings(1);
+speed.addRatings(5);
+speed.addRatings(1);
+console.log(speed.getAverageRating());
